d8d95ac28e816b6d38b2917b747edf8f43177dbf
Sneha Ghone <66209707+sneha1510@users.noreply.github.com>
2025-05-02 18:50:39 +0530

    Update HPC_3.cpp

--- HPC_3.cpp
+++ HPC_3.cpp
@@ -1,35 +1,36 @@
-#include <iostream>
-#include <vector>
-#include <omp.h>
-#include <climits> // For INT_MAX, INT_MIN
-
+#include<omp.h>
+#include<iostream>
+#include<vector>
 using namespace std;
-
-int main() {
-    vector<int> numbers = {10, 20, 5, 8, 35, 15};
-
-    int min_val = INT_MAX;
-    int max_val = INT_MIN;
-    int sum_val = 0;
-     
-    for(int n:numbers)
+int main()
+{
+    int n;
+    cout<<"Enter total no: ";
+    cin>>n;
+    vector<int> arr(n);
+    for(int i=0;i<n;i++)
     {
-      cout<<n<<" ";
+        cout<<"Enter "<<(i+1)<<" element: ";
+        cin>>arr[i];
     }
-    cout<<"\n";
-    #pragma omp parallel for reduction(min:min_val) reduction(max:max_val) reduction(+:sum_val)
-    for (int i = 0; i < numbers.size(); i++) {
-        min_val = min(min_val, numbers[i]);
-        max_val = max(max_val, numbers[i]);
-        sum_val += numbers[i];
-    }
+    cout<<"You have entered: ";
+    for(int i:arr)
+        cout<<i<<" ";
 
-    double avg_val = static_cast<double>(sum_val) / numbers.size();
-    
-    cout << "Min: " << min_val << endl;
-    cout << "Max: " << max_val << endl;
-    cout << "Sum: " << sum_val << endl;
-    cout << "Average: " << avg_val << endl;
+    cout<<endl;
+    int max_val=arr[0],min_val=arr[0],sum=0;
+    #pragma omp parallel for reduction(min:min_val) reduction(max:max_val) reduction(+:sum)
+    for(int i=0;i<arr.size();i++)
+    {
+        min_val=min(min_val,arr[i]);
+        max_val=max(max_val,arr[i]);
+        sum+=arr[i];
+    }
+    double avg=static_cast<double>(sum)/arr.size();
+    cout<<"Min value: "<<min_val<<endl;
+    cout<<"Max value: "<<max_val<<endl;
+    cout<<"Sum: "<<sum<<endl;
+    cout<<"avg: "<<avg<<endl;
 
     return 0;
 }
